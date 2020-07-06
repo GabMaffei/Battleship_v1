a8c1c00eb1586bf26a1d2fca29c2910acd1d9f3d
Gabriel Maffei de Almeida <gabriel.maffei.alm@gmail.com>
2020-07-06 18:52:59 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <stdlib.h> ///Biblioteca a incluir para gerar nœmeros aleatórios
+#include <time.h>   ///Biblioteca a incluir para gerar nœmeros aleatórios
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int numero;
+
+    srand((unsigned)time(NULL) ); ///Comando obrigatório antes de gerar nœmeros aleatórios
+
+    ///gerando numero aleatório
+    //numero = rand()%10-5;  ///o comando rand() gera o numero aleatório o mod delimita o intervalo, neste caso de 0 a 9. O +1 no final garante a geração de 1 a 10
+    //cout << numero << endl;
+
+    ///gerando novo numero
+    numero = rand()%10-5;
+    cout << numero << endl;
+
+    return 0;
+}
